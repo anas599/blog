bc8ac49dc4b7375443aaad117963403ce73ba959
anas599 <anas599@yahoo.com>
2023-05-02 12:02:43 +0300

    Add methods for user to read && update counters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,34 @@
 class User < ApplicationRecord
+  has_many :posts
+  def find_posts(author_id)
+    Post.where(author_id:).order(created_at: :desc).limit(3)
+  end
+
+  def count_post
+    Post.count
+  end
+
+  def update_posts_counter
+    update(posts_counter: count_post)
+  end
+
+  def recent_5_comments
+    Comment.where(author_id: id).order(created_at: :desc).limit(5)
+  end
+
+  def comment_counter_post
+    Comment.count
+  end
+
+  def update_comments_counter
+    update(comments_counter: comment_counter_post)
+  end
+
+  def likes_counter_post
+    Like.count
+  end
+
+  def update_likes_counter
+    update(likes_counter: likes_counter_post)
+  end
 end
