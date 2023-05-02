2001e8f7f00df1102482eb0f9eff67af807cc94e
anas599 <anas599@yahoo.com>
2023-05-02 16:06:44 +0300

    update comments model method for counting comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  def self.comments_counter(post)
+    where(post:).count
+  end
+
+  def self.update_comments_counter(post)
+    post.update(comments_counter: comments_counter(post))
+  end
 end
