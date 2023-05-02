182cc8dd5bd488ead1550228775e352c07d48166
anas599 <anas599@yahoo.com>
2023-05-02 15:41:55 +0300

    update posts counter and recent 5 comments method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,18 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+
+
+  def self.posts_counter(user)
+    where(author: user).count
+  end
+
+  def self.update_posts_counter(user)
+    User.select(:id).find(user.id).update(posts_counter: posts_counter(user))
+  end
+
+  def recent_5_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
