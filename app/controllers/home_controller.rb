22b6817cab2e55ce99aac90c3203c69ab912f470
anas599 <anas599@yahoo.com>
2023-05-04 16:18:59 +0300

    Add request specs

--- app/controllers/home_controller.rb
+++
@@ -1,5 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    @user = user_data
-  end
-end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,7 +3,6 @@ class UserController < ApplicationController
     @user = user_data.first
   end
 
-  #   def posts_show; end
   def index
     @user = user_data
   end
