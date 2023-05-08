e6bfcc224052468bbd8a094699906e9db978d182
anas599 <anas599@yahoo.com>
2023-05-08 15:58:50 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
+
   root 'user#index'
   get 'users/:id', to: 'user#show'
   get 'users/:id/posts', to: 'posts#index'
   get 'users/', to: 'user#index'
   get 'users/:id/posts/:id', to: 'posts#show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "new_post", to: "posts#new"
+  post "new_post", to: "posts#create"
+  get 'success', to: 'posts#success'
+
+  get "new_comment", to: "comment#new"
+  post "new_comment", to: "comment#create"
+  get "comment_success", to: "comment#comment_success"
+  
 end
