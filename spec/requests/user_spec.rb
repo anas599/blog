a5a8f842ac755992a4c8767cf40854f4145fa35b
anas599 <anas599@yahoo.com>
2023-05-13 16:10:50 +0300

    add capybara test for user/post

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -2,10 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'User Page', type: :system do
   it 'shows the user name' do
-    visit user_index_path
+    
+  visit user_index_path
     expect(page).to have_content('Number of posts')
     expect(page).to have_content('Tom')
     expect(page).to have_content('image')
-    sleep 5
+    sleep 3
+  end
+  it 'shows the user posts' do
+  visit user_path(42)
+    expect(page).to have_content('Bio')
+    expect(page).to have_content('Posts')
+    sleep 2
   end
 end
