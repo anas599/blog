b9ffaed15ee3541ebaed8522fd652c56915c3668
anas599 <anas599@yahoo.com>
2023-05-13 16:11:12 +0300

    fix rubocop linters

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -2,15 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'User Page', type: :system do
   it 'shows the user name' do
-    
-  visit user_index_path
+    visit user_index_path
     expect(page).to have_content('Number of posts')
     expect(page).to have_content('Tom')
     expect(page).to have_content('image')
     sleep 3
   end
   it 'shows the user posts' do
-  visit user_path(42)
+    visit user_path(42)
     expect(page).to have_content('Bio')
     expect(page).to have_content('Posts')
     sleep 2
