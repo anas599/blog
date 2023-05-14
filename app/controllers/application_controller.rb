8305566870a0a6c84559a075a9563081dbf77f8c
anas599 <anas599@yahoo.com>
2023-05-14 13:51:04 +0300

    Update devise current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,23 @@
 class ApplicationController < ActionController::Base
-  helper_method :user_data
-  helper_method :user_posts
-  helper_method :current_user
-  def user_data
-    @user_data ||= User.all
-  end
-
-  def user_posts
-    @user_posts ||= Post.all
-  end
-
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_current_user
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name posts_counter])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name posts_counter])
+  end
+
+  def set_current_user
+    @current_user = current_user
+  end
+
+  def user_data
+    @user_data ||= User.all
+  end
+
+  def user_posts
+    @user_posts ||= Post.all
   end
 end
