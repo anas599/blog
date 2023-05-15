864a6671b23a53ffad606bf4685c2c3336a66c46
anas599 <anas599@yahoo.com>
2023-05-15 18:28:02 +0300

    fix n+1 && add user capybara test

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -1,17 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe 'User Page', type: :system do
-  it 'shows the user name' do
+RSpec.describe 'User index/show', type: :system do
+  it 'Test User index' do
     visit user_index_path
+    expect(current_path).to eq(user_index_path)
     expect(page).to have_content('Number of posts')
     expect(page).to have_content('Tom')
     expect(page).to have_content('image')
-    sleep 3
+    sleep 1
+    click_link('Tom', exact_text: true)
+    sleep 1
+    expect(current_path).to eq(user_path(42))
+    sleep 2
   end
-  it 'shows the user posts' do
+  it 'Test User show' do
     visit user_path(42)
-    expect(page).to have_content('Bio')
-    expect(page).to have_content('Posts')
+    expect(current_path).to eq(user_path(42))
+    expect(page).to have_content('Tom')
+    expect(page).to have_content('image')
+    expect(page).to have_content('Number of posts')
+    # expect(page).to have_content('Comments')
+    # expect(page).to have_content('Likes')
     sleep 2
   end
 end
