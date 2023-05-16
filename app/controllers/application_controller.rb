b455d4ecfb8f662e3800d0f1a2dbccf4df1edb46
anas599 <anas599@yahoo.com>
2023-05-17 01:42:43 +0300

    remove checkbox to match wireframe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name password password_confirmation posts_counter])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name password password_confirmation posts_counter photo])
   end
 
   def set_current_user
