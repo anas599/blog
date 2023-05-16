3d40d6031563ce286fb2809ef609a4912de87047
hopeforkoami <hopeforkoami@gmail.com>
2023-05-16 20:45:04 +0000

    Add email confirmation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,15 @@
 class ApplicationController < ActionController::Base
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :set_current_user
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_current_user
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name posts_counter])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name posts_counter])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name password password_confirmation posts_counter])
+    #devise_parameter_sanitizer.permit(:sign_up, keys: %i[name posts_counter])
+    #devise_parameter_sanitizer.permit(:account_update, keys: %i[name posts_counter])
   end
 
   def set_current_user
