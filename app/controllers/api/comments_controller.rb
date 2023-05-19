6c2992d3669f3eb1a041ba8b7b0ba1b47811070a
hopeforkoami <hopeforkoami@gmail.com>
2023-05-19 15:54:50 +0000

    Add api create and find on comment

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -5,4 +5,27 @@ class Api::CommentsController < ApplicationController
     comments = posts.comments
     render json: comments
   end
+
+  
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.author = @auth_user
+    @comment.post = @post
+
+    if @comment.save
+      render json: { status: 'Success', data: @comment }
+    else
+      render json: { status: 'Failure', error: @comment.errors.full_messages }
+    end
+  end
+
+  private
+
+  def find_post
+    @post = Post.find_by_id(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
