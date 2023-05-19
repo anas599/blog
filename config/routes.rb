11de718a24f2c99debe8908eb2c4be615feb40f9
anas599 <anas599@yahoo.com>
2023-05-19 12:51:56 +0300

    add api post controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,22 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'user#index'
-     resources :user, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create, :destroy ] do
+
+  resources :user, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:create]
     end
+ end
+
+  namespace :api do
+    resources :user, only: [] do
+      resources :posts, only: [:index, :show] do
+        resources :comments, only: [:index, :show]
+      end
+    end
   end
+
   get "new_post", to: "posts#new"
   post "new_post", to: "posts#create"
   get 'success', to: 'posts#success'
@@ -16,6 +26,4 @@ Rails.application.routes.draw do
   get "new_comment", to: "comment#new"
   get 'destroy_comment', to: 'comment#destroy'
   post "new_comment", to: "comment#create"
-  # get "comment_success", to: "comment#comment_success"
-  
 end
