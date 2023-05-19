618ce34c441740ea751f374be53c52a7cd664329
hopeforkoami <hopeforkoami@gmail.com>
2023-05-19 15:56:34 +0000

    Add api route for create comment

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :user, only: [] do
       resources :posts, only: [:index, :show] do
-        resources :comments, only: [:index, :show]
+        resources :comments, only: [:index, :show,:create]
       end
     end
   end
